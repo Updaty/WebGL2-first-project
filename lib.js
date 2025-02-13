e2e26ec08de39f407cbf548138383ce16a15887e
Updaty <howdaytoday@gmail.com>
2025-02-13 19:25:05 +0100

    Activating a http server, the dream of a distinct GLSL file is broken.

--- lib.js
+++ lib.js
@@ -1,7 +1,4 @@
 'use strict';
-import shader from "./vec.glsl?raw";
-
-console.log(shader)
 
 const vertexShaderSource = (`#version 300 es
  
@@ -39,6 +36,23 @@ void main() {
 	outColor = u_color;
 }
 `);
+
+const errorRE = /ERROR:\s*\d+:(\d+)/gi;
+function addLineNumbersWithError(src, log = '') {
+  // Note: Error message formats are not defined by any spec so this may or may not work.
+  const matches = [...log.matchAll(errorRE)];
+  const lineNoToErrorMap = new Map(matches.map((m, ndx) => {
+    const lineNo = parseInt(m[1]);
+    const next = matches[ndx + 1];
+    const end = next ? next.index : log.length;
+    const msg = log.substring(m.index, end);
+    return [lineNo - 1, msg];
+  }));
+  return src.split('\n').map((line, lineNo) => {
+    const err = lineNoToErrorMap.get(lineNo);
+    return `${lineNo + 1}: ${line}${err ? `\n\n^^^ ${err}` : ''}`;
+  }).join('\n');
+}
 
 function randomInt(range) {
   return Math.floor(Math.random() * range);
